f4302c717cb4c9fd305c7e129f23b5f3184418c9
Khandakar Aryanul Haque <123832026+aryanul@users.noreply.github.com>
2023-12-02 19:41:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express ();
-var items = [];
+const date= require(__dirname+"/date.js")
+let items = [];
+let workItems= [];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-    var today = new Date();
-    var options= {
-      weekday:"long",
-      day:"numeric",
-      month:"long",
-    }
-    var day = today.toLocaleDateString("en-IN",options);
+    let day = date.getDate();
 
-    res.render('list', {kindOfDay:day, newItems:items});
+    res.render('list', {listTitle:day, newItems:items});
 });
 app.post("/", function(req,res){
-  var item = req.body.newItem;
+  let item = req.body.newItem;
+  if(req.body.list === "Work List"){
+    workItems.push(item);
+    res.redirect("/work");
+  }else{
   items.push(item);
   // console.log(newItem);
   // res.send("New task added!");
   res.redirect("/");
+  }
+})
+app.get("/work",function(req,res){
+  res.render("list",{listTitle:"Work List", newItems:workItems});
+})
+// app.post("/work",function(req,res){
+//   let item = req.body.newItem;
+//   workItems.push(item);
+//   res.redirect("/work");
+// })
+app.get("/about",function(req,res){
+  res.render("about");
 })
 app. listen(3000, function( ) {
     console. log("Server started on port 3000");
